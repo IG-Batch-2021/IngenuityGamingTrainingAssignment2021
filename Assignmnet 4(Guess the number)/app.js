5e1105ab2f18fecd99ec204762892c64e3526924
sksketer <amrit.agarwal_cs18@gla.ac.in>
2021-10-26 20:24:58 +0530

    Update app.js

--- Assignmnet 4(Guess the number)/app.js
+++ Assignmnet 4(Guess the number)/app.js
@@ -13,10 +13,14 @@ btn.addEventListener('click', function() {
     result.innerHTML = "";
     if(userNumber == randomNumber) {
         result.innerHTML = "Guess is correct.";
+        document.getElementById('result').style = "color: darkgreen; background: rgb(193, 255, 193);";
+        document.getElementById('container').style.background = "linear-gradient(360deg, darkgreen, lightgreen);";
     } else if(userNumber > randomNumber) {
         result.innerHTML = "Guess is large.";
+        document.getElementById('result').style = "color: darkgreen; background: rgb(193, 255, 193);";
     } else if(userNumber < randomNumber) {
         result.innerHTML = "Guess is small.";
+        document.getElementById('conatiner').style.background =  "linear-gradient(360deg, darkred, rgb(255, 198, 198))";
     } else {
         result.innerHTML = "Something goes Wrongs.";
     }
