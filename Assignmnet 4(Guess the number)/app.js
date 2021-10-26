b9e6f41b44c90d495028b246873f04a1d23cd1cd
sksketer <amrit.agarwal_cs18@gla.ac.in>
2021-10-26 22:32:20 +0530

    Update app.js
    change background color according to result

--- Assignmnet 4(Guess the number)/app.js
+++ Assignmnet 4(Guess the number)/app.js
@@ -1,27 +1,41 @@
-const randomNumber = 45;
+const randomNumber = Math.floor(Math.random()*100)
+console.warn(randomNumber)
 
 const one = document.querySelector('#one');
-const two = document.getElementById("two");
+// const two = document.getElementById("two");
 const result = document.querySelector("#result");
 
 const btn = document.querySelector("#playbtn");
 
-
 btn.addEventListener('click', function() {
-    var userNumber = one.value+two.value;
+    var userNumber = one.value;
 
     result.innerHTML = "";
     if(userNumber == randomNumber) {
         result.innerHTML = "Guess is correct.";
-        document.getElementById('result').style = "color: darkgreen; background: rgb(193, 255, 193);";
-        document.getElementById('container').style.background = "linear-gradient(360deg, darkgreen, lightgreen);";
+        document.getElementById('result').style = "color: darkgreen; background: rgb(193, 255, 193); margin: 22px 0px;";
+        document.body.style.background = "green";
+        // document.getElementById('container').style.background = "lightgreen";
     } else if(userNumber > randomNumber) {
         result.innerHTML = "Guess is large.";
-        document.getElementById('result').style = "color: darkgreen; background: rgb(193, 255, 193);";
+        document.getElementById('result').style = "color: darkred; background: rgb(255, 163, 163);";
+        // document.getElementById('conatiner').style.background =  "linear-gradient(360deg, darkred, rgb(255, 198, 198))";
+        document.body.style.background = "rgb(145, 0, 0)";
     } else if(userNumber < randomNumber) {
         result.innerHTML = "Guess is small.";
-        document.getElementById('conatiner').style.background =  "linear-gradient(360deg, darkred, rgb(255, 198, 198))";
+        document.getElementById('result').style = "color: darkred; background: rgb(255, 163, 163);";
+        // document.getElementById('conatiner').style.background =  "linear-gradient(360deg, darkred, rgb(255, 198, 198))";
+        document.body.style.background = "rgb(255, 87, 87)";
     } else {
         result.innerHTML = "Something goes Wrongs.";
     }
 })
+function tempAlert(msg,duration) {
+    var el = document.createElement("div");
+    el.setAttribute("style","position:absolute; top:75%; left:55%; background-color:white;");
+    el.innerHTML = msg;
+    setTimeout(function(){
+    el.parentNode.removeChild(el);
+    },duration);
+    document.body.appendChild(el);
+}
