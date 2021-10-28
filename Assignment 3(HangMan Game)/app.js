70fe2ae091365daa344004b75bcbe8f73ad235f2
Amrit Agarwal <54416489+sksketer@users.noreply.github.com>
2021-10-28 10:17:39 +0530

    Update app.js

--- Assignment 3(HangMan Game)/app.js
+++ Assignment 3(HangMan Game)/app.js
@@ -1,7 +1,141 @@
-const btnA = document.getElementById('cardA');
-var input = "key is press: ";
+// {
+    const btnA = document.getElementById('cardA');
+    const btnB = document.getElementById('cardB');
+    const btnC = document.getElementById('cardC');
+    const btnD = document.getElementById('cardD');
+    const btnE = document.getElementById('cardE');
+    const btnF = document.getElementById('cardF');
+    const btnG = document.getElementById('cardG');
+    const btnH = document.getElementById('cardH');
+    const btnI = document.getElementById('cardI');
+    const btnJ = document.getElementById('cardJ');
+    const btnK = document.getElementById('cardK');
+    const btnL = document.getElementById('cardL');
+    const btnM = document.getElementById('cardM');
+    const btnN = document.getElementById('cardN');
+    const btnO = document.getElementById('cardO');
+    const btnP = document.getElementById('cardP');
+    const btnQ = document.getElementById('cardQ');
+    const btnR = document.getElementById('cardR');
+    const btnS = document.getElementById('cardS');
+    const btnT = document.getElementById('cardT');
+    const btnU = document.getElementById('cardU');
+    const btnV = document.getElementById('cardV');
+    const btnW = document.getElementById('cardW');
+    const btnX = document.getElementById('cardX');
+    const btnY = document.getElementById('cardY');
+    const btnZ = document.getElementById('cardZ');
+// }
+const word = "sketeramrit"
+var input = "";
+for(let i=0; i<word.length; i++) {
+    input += "_ ";
+}
 
 btnA.addEventListener("click", function() {
-    input += "A ";
+    input += btnA.value;
     console.log(input)
 })
+btnB.addEventListener("click", function() {
+    input += btnB.value;
+    console.log(input)
+})
+btnC.addEventListener("click", function() {
+    input += btnC.value;
+    console.log(input)
+})
+btnD.addEventListener("click", function() {
+    input += btnD.value;
+    console.log(input)
+})
+btnE.addEventListener("click", function() {
+    input += btnE.value;
+    console.log(input)
+})
+btnF.addEventListener("click", function() {
+    input += btnF.value;
+    console.log(input)
+})
+btnG.addEventListener("click", function() {
+    input += btnG.value;
+    console.log(input)
+})
+btnH.addEventListener("click", function() {
+    input += btnH.value;
+    console.log(input)
+})
+btnI.addEventListener("click", function() {
+    input += btnI.value;
+    console.log(input)
+})
+btnJ.addEventListener("click", function() {
+    input += btnJ.value;
+    console.log(input)
+})
+btnK.addEventListener("click", function() {
+    input += btnK.value;
+    console.log(input)
+})
+btnL.addEventListener("click", function() {
+    input += btnL.value;
+    console.log(input)
+})
+btnM.addEventListener("click", function() {
+    input += btnM.value;
+    console.log(input)
+})
+btnN.addEventListener("click", function() {
+    input += btnN.value;
+    console.log(input)
+})
+btnO.addEventListener("click", function() {
+    input += btnO.value;
+    console.log(input)
+})
+btnP.addEventListener("click", function() {
+    input += btnP.value;
+    console.log(input)
+})
+btnQ.addEventListener("click", function() {
+    input += btnQ.value;
+    console.log(input)
+})
+btnR.addEventListener("click", function() {
+    input += btnR.value;
+    console.log(input)
+})
+btnS.addEventListener("click", function() {
+    input += btnS.value;
+    console.log(input)
+})
+btnT.addEventListener("click", function() {
+    input += btnT.value;
+    console.log(input)
+})
+btnU.addEventListener("click", function() {
+    input += btnU.value;
+    console.log(input)
+})
+btnV.addEventListener("click", function() {
+    input += btnV.value;
+    console.log(input)
+})
+btnW.addEventListener("click", function() {
+    input += btnW.value;
+    console.log(input)
+})
+btnX.addEventListener("click", function() {
+    input += btnX.value;
+    console.log(input)
+})
+btnY.addEventListener("click", function() {
+    input += btnY.value;
+    console.log(input)
+})
+btnZ.addEventListener("click", function() {
+    input += btnZ.value;
+    console.log(input)
+})
+function findWord(str) {
+    
+}
