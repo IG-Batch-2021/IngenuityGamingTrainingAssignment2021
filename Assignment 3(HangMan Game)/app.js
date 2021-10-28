cf98b69af58a6bc89a4f5aafc37788b88cd81fd8
sksketer <amrit.agarwal_cs18@gla.ac.in>
2021-10-28 10:44:19 +0530

    Update app.js

--- Assignment 3(HangMan Game)/app.js
+++ Assignment 3(HangMan Game)/app.js
@@ -1,7 +1,15 @@
-const btnA = document.getElementById('cardA');
 var input = "key is press: ";
 
-btnA.addEventListener("click", function() {
-    input += "A ";
-    console.log(input)
-})
+let btnArr = [];
+
+for(let i = 65; i <= 90; i++) {
+    const btn = document.getElementById(`card${String.fromCharCode(i)}`);
+    btn.addEventListener("click", onAlphabetClick)
+    btnArr.push(btn);
+}
+
+
+function onAlphabetClick(e) {
+    console.log(e)
+    console.log(e.srcElement.innerText);
+}
