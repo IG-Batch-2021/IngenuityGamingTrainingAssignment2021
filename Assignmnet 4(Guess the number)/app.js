22c88960aff00762077f17190ef40650fedd41c6
sksketer <amrit.agarwal_cs18@gla.ac.in>
2021-10-29 12:04:25 +0530

    add sound effect in win and loss game

--- Assignmnet 4(Guess the number)/app.js
+++ Assignmnet 4(Guess the number)/app.js
@@ -9,6 +9,12 @@ const btn = document.querySelector("#playbtn");
 const countAttempt = document.querySelector('#count');
 var count = 0;
 
+window.onload = (event) => {
+    document.getElementById('one').value = ""
+}
+var winSound = new Audio('sound/win.mp3');
+var tryagainSound = new Audio('sound/tryagain.mp3');
+
 btn.addEventListener('click', function() {
     count++;
     countAttempt.innerHTML = count;
@@ -16,24 +22,31 @@ btn.addEventListener('click', function() {
 
     result.innerHTML = "";
     if(userNumber == randomNumber) {
-        result.innerHTML = "Guess is correct.";
+        result.innerHTML = `Guess is correct in ${count} Attemps`;
         document.getElementById('result').style = "color: darkgreen; background: rgb(193, 255, 193); margin: 22px 0px;";
         document.body.style.background = "green";
+        document.getElementById('one').style.pointerEvents = "none";
+        btn.style.display = "none";
+        winSound.play();
+
         // document.getElementById('container').style.background = "lightgreen";
     } else if(userNumber > randomNumber) {
         result.innerHTML = "Guess is large.";
         document.getElementById('result').style = "color: darkred; background: rgb(255, 163, 163);";
         // document.getElementById('conatiner').style.background =  "linear-gradient(360deg, darkred, rgb(255, 198, 198))";
         document.body.style.background = "rgb(145, 0, 0)";
+        tryagainSound.play();
     } else if(userNumber < randomNumber) {
         result.innerHTML = "Guess is small.";
         document.getElementById('result').style = "color: darkred; background: rgb(255, 163, 163);";
         // document.getElementById('conatiner').style.background =  "linear-gradient(360deg, darkred, rgb(255, 198, 198))";
         document.body.style.background = "rgb(255, 87, 87)";
+        tryagainSound.play();
     } else {
         result.innerHTML = "Something goes Wrongs.";
     }
 })
+
 function tempAlert(msg,duration) {
     var el = document.createElement("div");
     el.setAttribute("style","position:absolute; top:75%; left:55%; background-color:white;");
