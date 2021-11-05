79a7e58099b1e2309ba9421faedf9b8701ebd5a0
sksketer <amrit.agarwal_cs18@gla.ac.in>
2021-11-06 01:53:51 +0530

    added main game logics

--- Assignment 6(Snake Game)/app.js
+++ Assignment 6(Snake Game)/app.js
@@ -13,3 +13,52 @@ var foodX = foodY = 29; //food staring position
 var xVel = yVel = 0;
 var trail = [];
 var tail = 5;
+
+window.onload=function() {
+    document.addEventListener("keydown", keyPush);
+    setInterval(game,1000/15);
+}
+
+const game = () => {
+    snakeX += xVel;
+    snakeY += yVel;
+
+    if(snakeX < 0 ) {
+        snakeX = tileCount-1;
+    }
+
+    if(snakeX > tileCount-1 ) {
+        snakeX = 0;
+    }
+    if(snakeY < 0 ) {
+        snakeY = tileCount-1;
+    }
+    if(snakeY > tileCount-1 ) {
+        snakeY=0;
+    }
+
+    ctx.fillStyle = boardColor;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = snakeColor;
+
+    for(let i=0; i<trail.length; i++) {
+        ctx.fillRect(trail[i].x*gridSize, trail[i].y*gridSize, gridSize-2, gridSize-2)
+        if(trail[i].x == snakeX && trail[i].y == snakeY) {
+        tail = 5;
+        }
+    }
+
+    trail.push({x:snakeX, y:snakeY});
+    while(trail.length > tail) {
+        trail.shift();
+    }
+
+    if(foodX == snakeX && foodY == snakeY) {
+        tail++;
+        foodX = Math.floor((Math.random() * tileCount));
+        foodY = Math.floor((Math.random() * tileCount));
+    }
+
+    ctx.fillStyle = 'yellow'; //food color
+    ctx.fillRect(foodX*gridSize, foodY*gridSize, gridSize-2, gridSize-2);
+}
