fbfd33ccc33666a499df04d9f569341c282446f1
Amrit Agarwal <54416489+sksketer@users.noreply.github.com>
2021-11-12 15:04:23 +0530

    add draw rectangle

--- Assignment 7(paintingWebTool)/app.js
+++ Assignment 7(paintingWebTool)/app.js
@@ -4,7 +4,20 @@ const rectangle = document.getElementById('rectangle');
 const circle = document.getElementById('circle');
 const color = document.getElementById('selectColor');
 
-const canvas = document.getElementById('paintArea');
+let canvas = document.getElementById('paintArea');
+canvas.style.width ='100%';
+canvas.style.height='100%';
+canvas.width  = canvas.offsetWidth;
+canvas.height = canvas.offsetHeight;
+let c = canvas.getContext('2d');
+
+let rectangleCordinates = [];
+
+let strokeColor = "black";
+let fillColor = "transparent";
+
+c.fillStyle = fillColor;
+c.strokeStyle = strokeColor;
 
 brush.addEventListener("click", function() {
     console.log("Brush is seleced.");
@@ -13,6 +26,7 @@ brush.addEventListener("click", function() {
     document.getElementById('rectangle').style.color = 'black';
     document.getElementById('circle').style.color = 'black';
 })
+
 ruler.addEventListener("click", function() {
     console.log("Ruler is selected.");
     document.getElementById('brush').style.color = 'black';
@@ -20,13 +34,16 @@ ruler.addEventListener("click", function() {
     document.getElementById('rectangle').style.color = 'black';
     document.getElementById('circle').style.color = 'black';
 })
+
 rectangle.addEventListener("click", function() {
     console.log("Rectangle is selected.");
     document.getElementById('brush').style.color = 'black';
     document.getElementById('ruler').style.color = 'black';
     document.getElementById('rectangle').style.color = 'blue';
     document.getElementById('circle').style.color = 'black';
+    
 })
+
 circle.addEventListener("click", function() {
     console.log("Circle is selected.");
     document.getElementById('brush').style.color = 'black';
@@ -34,6 +51,32 @@ circle.addEventListener("click", function() {
     document.getElementById('rectangle').style.color = 'black';
     document.getElementById('circle').style.color = 'blue';
 })
+
 color.addEventListener("click", function() {
-    console.log(document.getElementById('selectColor').value);
+    strokeColor = color.value;
+    console.log(strokeColor);
+    console.log(fillColor);
+    c.fillStyle = fillColor;
+    c.strokeStyle = strokeColor;
 })
+
+// canvas.addEventListener("mousemove", getCordinates(event));
+let x, y;
+function getCordinates(event) {
+    x = event.clientX - 62;
+    y = event.clientY - 10;
+    
+    console.log("cordinates: "+x+" "+y);
+    // console.log((x)+" "+(y))
+}
+function getWidthAndHeight(event) {
+    var width = event.clientX - x;
+    var height = event.clientY - y;
+
+    console.log("width: "+width+" Height: "+height);
+    c.fillRect(x, y, width, height);
+    c.strokeRect(x, y, width, height);
+    console.log(x+" "+y+" "+width+" "+height)
+}
+
+// c.fillRect(0, 0, 150, 100);
