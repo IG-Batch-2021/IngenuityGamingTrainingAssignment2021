e099aa3aa1b9261fc009ec55dcdc8e1f348f87fc
sksketer <amrit.agarwal_cs18@gla.ac.in>
2021-11-13 00:24:22 +0530

    Update app.js

--- Assignment 7(paintingWebTool)/app.js
+++ Assignment 7(paintingWebTool)/app.js
@@ -1,8 +1,10 @@
+const clearAll = document.getElementById('clearCanvas');
 const brush = document.getElementById('brush');
 const ruler = document.getElementById('ruler');
 const rectangle = document.getElementById('rectangle');
 const circle = document.getElementById('circle');
 const color = document.getElementById('strokeColor');
+const main = document.getElementById('main');
 
 let canvas = document.getElementById('paintArea');
 canvas.style.width ='100%';
@@ -20,6 +22,11 @@ let selectBrush = selectRuler = selectRect = selectCircle = false;
 
 c.fillStyle = fillColor;
 c.strokeStyle = strokeColor;
+
+clearAll.addEventListener("click", function() {
+    console.log("canvas clean");
+    c.clearRect(0, 0, canvas.width, canvas.height);
+});
 
 brush.addEventListener("click", function() {
     console.log("Brush is seleced.");
@@ -70,20 +77,34 @@ color.addEventListener("click", function() {
 })
 
 // canvas.addEventListener("mousemove", getCordinates(event));
-let x, y, width, height;
+function showLiveCordinates(event) {
+    let livePosition = ("( "+(event.clientX-62)+" , "+(event.clientY-10)+" )");
+    document.getElementById('showLiveCursorPosition').innerHTML = livePosition;
+}
+let x = y = width = height = checkEndpoint = 0;
 
-function getCordinates(event) {
+function getStartCordinates(event) {
     x = event.clientX - 62;
     y = event.clientY - 10;
-    
-    console.log("cordinates: "+x+" "+y);
+    main.addEventListener("mousemove", showShape(event));
+    // document.onmousemove = showShape(event);
+    console.log("Start cordinates: "+x+" "+y);
 }
-function getWidthAndHeight(event) {
-    width = event.clientX - x;
-    height = event.clientY - y;
 
-    console.log("width: "+width+" Height: "+height);
-    // console.log(x+" "+y+" "+width+" "+height)
+function showShape(event) {
+    // console.log("fig show: "+event.clientX+" "+event.clientY);
+    let shape = "none";
+    if(selectRect) shape = "selectRect";
+    else if(selectRuler) shape = "selectRuler";
+    else if(selectCircle) shape = "selectCircle";
+    else if(selectBrush) shape = "selectBrush";
+    drawShape(shape,x, y, event.clientX-x-62, event.clientY-y-10);
+}
+function getEndCordinates(event) {
+    width = event.clientX - x - 62;
+    height = event.clientY - y - 10;
+
+    console.log("End cordinate: "+(width+x)+" "+(height+y))
     let shape = "none";
     if(selectRect) shape = "selectRect";
     else if(selectRuler) shape = "selectRuler";
@@ -93,54 +114,79 @@ function getWidthAndHeight(event) {
 }
 
 function drawShape(shape, x, y, width, height) {
-    console.log(shape+" function ");
+    // console.log(shape+" function ");
     switch(shape) {
         case "selectBrush":
             drawBrush(x, y, width, height);
             break;
         case "selectRuler":
-            drawRuler();
+            drawRuler(x, y, width, height);
             break;
         case "selectRect":
-            drwaRect(x, y, width, height);
+            drawRect(x, y, width, height);
             break;
         case "selectCircle":
             drawCircle(x, y, width, height);
             break;
         default:
             alert("select any tool");
-            break;
-        
+            break;        
     }
-}
-
-function drwaRect(dx, dy, width, height) {
-    c.fillRect(dx, dy, width, height);
-    c.strokeRect(dx, dy, width, height);
 }
 
 function drawBrush(dx, dy, width, height) {
     let radius = 5;
-    canvas.addEventListener("onmove");
+    console.log("brush works");
+    canvas.addEventListener("onmousemove", function() {
+        c.beginPath();
+        c.arc(x, y, radius, 0, 2 * Math.PI);
+        c.stroke();
+        c.closePath();
+        console.log("brush draw sucessfully");
+        console.log(dx+" "+dy+" "+width+" "+height);
+    })
+}
+
+function drawRuler(x, y, width, heigth) {
+    console.log(x+" "+y);
+    c.lineWidth = 2;
+    let x2 = width+x;
+    let y2 = height+y;
+    console.log("start")
     c.beginPath();
-    c.arc(x, y, radius, 0, 2 * Math.PI);
+    c.moveTo(x, y);
+    c.lineTo(x2, y2);
     c.stroke();
     c.closePath();
-    console.log("circle draw sucssfully");
-    console.log(dx+" "+dy+" "+width+" "+height);
+    console.log("end")
+}
+
+function drawRect(dx, dy, width, height) {
+    dx -= 21;
+    console.log("x: "+dx+" y: "+dy);
+    console.log("width: "+width+" Height: "+height);
+    c.fillRect(dx, dy, width, height);
+    c.strokeRect(dx, dy, width, height);
 }
 
 function drawCircle(dx, dy, width, height) {
-    let radius = width;
+    let radius = findRadius(width, height);
+    if(width == 0) radius = height;
     c.beginPath();
-    c.arc(x, y, radius, 0, 2 * Math.PI);
+    c.arc(x, y, Math.abs(radius), 0, 2 * Math.PI);
     c.stroke();
-    c.closePath();    
+    c.closePath(); 
+    console.log("Radius: "+Math.abs(radius));
 }
 
-function drawRuler() {
+function findRadius(width, height) {
+    let p = width * width;
+    let b = height * height;
+    let sum = p+b;
+    let h = Math.sqrt(sum, 2);
+    console.log(p+" "+b+" "+h);
 
+    return h;
 }
-
 
 // c.fillRect(0, 0, 150, 100);
