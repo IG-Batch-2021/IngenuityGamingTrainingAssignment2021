ecbf1e037eb7f24aea7b4afb01105c4023ffac08
Amrit Agarwal <54416489+sksketer@users.noreply.github.com>
2021-11-17 12:44:19 +0530

    Update app.d.ts

--- Assignment 8(Covid-Data)/js/app.d.ts
+++ Assignment 8(Covid-Data)/js/app.d.ts
@@ -1,3 +1,4 @@
 declare const searchBtn: HTMLButtonElement;
 declare const getAllData: (url: string, country: string, method: string, header: Headers) => Promise<any>;
+declare function filterData(arg0: Promise<any>): any;
 //# sourceMappingURL=app.d.ts.map
