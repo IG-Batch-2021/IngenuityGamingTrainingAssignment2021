caad8bbf7854430ed571456da6bd966864e1b837
Amrit Agarwal <54416489+sksketer@users.noreply.github.com>
2021-11-17 16:19:12 +0530

    Update getAPIData.ts

--- Assignment 8(Covid-Data)/ts/getAPIData.ts
+++ Assignment 8(Covid-Data)/ts/getAPIData.ts
@@ -25,8 +25,8 @@
             "headers": header
         })
         .then(response => {
-            console.log(response.json());
-            // console.log(filterData(response.json()));
+            // console.log(response.json());
+            console.log(filterData(response.json()));
         })
         .catch(err => {
             console.error(err);
@@ -35,8 +35,19 @@
     }
 
 
-function filterData(arg0: Promise<any>): any {
-    throw new Error("Function not implemented.");
-    // console.log("hello json data")
+function filterData(data: Promise<any>): any {
+    let length;
+    printAddress();
+
+    function printAddress() {
+        data.then((a) => {
+            console.log("-------------")
+            // console.log(a);
+            for(let i=0; i<a.length; i++) {
+                console.log("id: "+a[i].id+" Rank: "+a[i].rank+" ActiveCases: "+a[i].ActiveCases+" Case Fatality Rate: "+a[i].Case_Fatality_Rate+" Continent: "+a[i].Continent+" Contry: "+a[i].Country);
+            }
+            console.log("-------------")
+        });
+    }
 }
 // }
